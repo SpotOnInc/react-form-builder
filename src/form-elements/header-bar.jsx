3bda5e1f6a59c15bef681f4041beed3c88bd56fa
darthvoider <voiderok@gmail.com>
2022-04-22 17:35:08 +0300

    add submenu

--- src/form-elements/header-bar.jsx
+++ src/form-elements/header-bar.jsx
@@ -3,16 +3,49 @@
   */
 
 import React from 'react';
-import Grip from '../multi-column/grip';
 import DragHandle from './component-drag-handle';
+import useToggle from '../utils/hooks/useToggle';
+
+const HeaderMenu = () => {
+  const { isOpen, toggle } = useToggle();
+
+  const buttonList = [
+    {
+      icon: 'fas fa-arrows-alt',
+      text: 'Move',
+    },
+    {
+      icon: 'far fa-trash-alt',
+      text: 'Delete',
+    },
+  ];
+
+  return (
+    <div className="toolbar-header-buttons">
+      <button className="btn" onClick={toggle}>
+        <i className="fas fa-ellipsis-v" />
+      </button>
+      {isOpen && (
+        <div className='toolbar-header-submenu'>
+          {buttonList.map(({ icon, action, text }) => (
+            <button key={text} className="btn" onClick={action}>
+              <i className={icon} />
+              <span>{text}</span>
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default class HeaderBar extends React.Component {
-
   render() {
     return (
       <div className="toolbar-header">
         <span className="badge badge-secondary">{this.props.data.text}</span>
-        <div className="toolbar-header-buttons">
+        {this.props.showInlineEditForm && <HeaderMenu />}
+        {!this.props.showInlineEditForm && <div className="toolbar-header-buttons">
           {this.props.data.element !== 'LineBreak' &&
             <div className="btn is-isolated" onClick={this.props.editModeOn.bind(this.props.parent, this.props.data)}><i className="is-isolated fas fa-edit"></i></div>
           }
@@ -22,7 +55,7 @@ export default class HeaderBar extends React.Component {
           } */}
 
           <DragHandle data={this.props.data} index={this.props.index} onDestroy={this.props.onDestroy} setAsChild={this.props.setAsChild} />
-        </div>
+        </div>}
       </div>
     );
   }
