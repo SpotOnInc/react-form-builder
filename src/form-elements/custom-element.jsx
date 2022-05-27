f0dbda03779d6b60a848004d9244ad021b6d23d8
darthvoider <voiderok@gmail.com>
2022-05-27 12:24:28 +0300

    add draggable wrapper

--- src/form-elements/custom-element.jsx
+++ src/form-elements/custom-element.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ComponentHeader from './component-header';
 import ComponentLabel from './component-label';
+import DragHandle from './component-drag-handle';
 
 class CustomElement extends Component {
   constructor(props) {
@@ -29,7 +30,7 @@ class CustomElement extends Component {
     let baseClasses = 'SortableItem rfb-item';
     if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
 
-    return (
+    const renderCustomElementInner = () => (
       <div className={baseClasses}>
         {!this.props.showInlineEditForm && <ComponentHeader {...this.props} />}
         { bare ?
@@ -41,6 +42,18 @@ class CustomElement extends Component {
         }
       </div>
     );
+
+    return this.props.showInlineEditForm
+      ? <DragHandle
+          data={this.props.data}
+          index={this.props.index}
+          onDestroy={this.props.onDestroy}
+          setAsChild={this.props.setAsChild}
+          showInlineEditForm={this.props.showInlineEditForm}
+        >
+          {renderCustomElementInner()}
+        </DragHandle>
+      : renderCustomElementInner();
   }
 }
 
